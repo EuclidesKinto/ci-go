ed2da397c77600da2ec0977712551af705052b86
Euclides <euclideskinto@gmail.com>
2021-09-21 18:41:18 -0300

    feat: :sparkles: add func soma

---
+++ math_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSoma(t *testing.T) {
+	total := Soma(15, 15)
+
+	if total != 30 {
+		t.Errorf("Resultado da soma é inválido.O resultado é %d O Esperado é: %d", total, 30)
+	}
+}
